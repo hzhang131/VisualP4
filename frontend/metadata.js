1f899948ac7974f73e19935dca44191f1d70626d
hzhang131 <hz13@illinois.edu>
2023-07-02 15:23:42 -0500

    7/2 Update

---
+++ frontend/metadata.js
@@ -0,0 +1,18 @@
+function addActionModuleStatements(node_id){
+    console.log('line 2');
+}
+function addStateModuleStatements(node_id){
+    console.log('line 5');
+}
+function addTableModuleStatements(node_id){
+    console.log('line 8');
+}
+function removeActionModuleStatements(node_id){
+    console.log('line 11');
+}
+function removeStateModuleStatements(node_id){
+    console.log('line 14');
+}
+function removeTableModuleStatements(node_id){
+    console.log('line 17');
+}
